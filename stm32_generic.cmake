b1906d940449c517f4b3661550946633d5d2e780
Félix Piédallu <felix@piedallu.me>
2017-04-02 21:03:03 +0200

    software reset through SWD

--- stm32_generic.cmake
+++ stm32_generic.cmake
@@ -4,6 +4,7 @@ add_custom_target(flash
     COMMAND arm-none-eabi-objcopy -Oihex $<TARGET_FILE:main_binary> $<TARGET_FILE:main_binary>.hex
     COMMAND echo "Flashing the STM32 Board !"
     COMMAND sudo openocd -f ${OpenOCD_CFG}
+            -c \"reset_config none separate\"
             -c \"init\"
             -c \"reset init\"
             -c \"flash write_image erase $<TARGET_FILE:main_binary>.hex\"
